5492cd3f761eeb04021737885917b52d43235bb4
CliffeAbudo <fortuneabudo@gmail.com>
2026-06-26 02:15:57 +0300

    Getting computer choice

---
+++ js_files/script.js
@@ -0,0 +1,15 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+  let randomNumber  = Math.random();
+
+if (randomNumber <= 0.33){
+    return "rock";
+}else if(randomNumber < 0.66){
+    return "paper";
+}else{
+    return "scissors";
+}
+}
+
