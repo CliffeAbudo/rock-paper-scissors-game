721c0f7688e80b4a8886bd3f0c9878a52c939216
CliffeAbudo <fortuneabudo@gmail.com>
2026-06-26 02:19:18 +0300

    Function for playing one round

--- js_files/script.js
+++ js_files/script.js
@@ -18,3 +18,36 @@ function getHumanChoice(){
    let answer = prompt("Pick a choice");
    return answer;
 }
+
+
+
+function playRound(humanChoice,computerChoice){
+
+    humanChoice=humanChoice.toLowerCase();
+    if (humanChoice === computerChoice){
+         console.log("This is a draw");
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors"){
+         humanScore ++ ;
+         console.log("Human wins!rock beats scissors");
+         
+    }
+    else if(humanChoice === "paper" && computerChoice === "rock"){
+         humanScore ++ ;
+         console.log("Human wins! paper beats rock");
+    }
+    else if(humanChoice === "scissors" && computerChoice === "paper"){
+         humanScore ++ ;
+         console.log("Human wins! scissors beats paper");
+    }
+    else{
+        computerScore ++ ;
+        console.log("Computer wins");
+         
+    }
+
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
